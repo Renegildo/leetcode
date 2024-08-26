44b7b9dcaf863422961fe55519ee35bed322d295
Renegildo <119348022+Renegildo@users.noreply.github.com>
2024-08-26 14:05:55 -0300

    Time: 46 ms, Memory: 54.8 MB - LeetHub

---
+++ 0max-consecutive-ones/0max-consecutive-ones.ts
@@ -0,0 +1,17 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let max = 0;
+    let cur = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            cur++;
+            if (cur > max) {
+                max = cur;
+            }
+        } else {
+            cur = 0;
+        }
+    }
+    
+    return max;
+};
